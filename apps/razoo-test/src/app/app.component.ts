ec1b0f346854afce3ec4a6e2f9bd6ebc8f06d849
96dev <38684729+96dev@users.noreply.github.com>
2023-08-26 18:52:01 +0530

    Create starter files for angular-16.2.0

---
+++ apps/razoo-test/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'razoo-test-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'razoo-test';
+}
